9b8a3e29c3e2539908cbdec06dc18f17b4083646
MarwanElAbhar <marwan@aklat.io>
2024-11-08 19:39:59 +0200

    initalize project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">Event Booking App</h2>
+
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
+
+  </main>
+</template>
